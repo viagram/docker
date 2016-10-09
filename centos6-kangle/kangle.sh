27714bc9ca2e00cefb6b7a2b306162b057390fc1
viagram <viagram.yang@gmail.com>
2016-10-09 12:30:48 +0800

    20161009

--- centos6-kangle/kangle.sh
+++ centos6-kangle/kangle.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 service kangle stop
+cur_dir=`pwd`/shadowsocks-R
+if [ ! -d "${cur_dir}" ]; then
+    mkdir -p ${cur_dir}
+fi
+cd ${cur_dir}
 rm -rf kangle*
 yum -y install make automake gcc gcc-c++ pcre-devel zlib-devel sqlite-devel openssl-devel
 wget --no-check-certificate https://raw.githubusercontent.com/viagram/docker/master/centos6-kangle/kangle-3.5.8.tar.gz -O kangle-3.5.8.tar.gz
@@ -9,3 +15,7 @@ cd kangle-3.5.8
 make
 make install
 service kangle start
+cd ${cur_dir}
+cd ..
+rm -rf ${cur_dir}
+rm -f $0
