7db4ab0796940d4e60aa207a375c6cc96e0fd34b
viagram <viagram.yang@gmail.com>
2016-10-10 19:48:44 +0800

    20161010

--- centos6-kangle/kangle.sh
+++ centos6-kangle/kangle.sh
@@ -8,13 +8,14 @@ fi
 cd ${cur_dir}
 rm -rf kangle*
 yum -y install make automake gcc gcc-c++ pcre-devel zlib-devel sqlite-devel openssl-devel
-wget --no-check-certificate https://raw.githubusercontent.com/viagram/docker/master/centos6-kangle/kangle-3.5.8.tar.gz -O kangle-3.5.8.tar.gz
+wget --no-check-certificate https://raw.githubusercontent.com/viagram/docker/master/centos6-kangle/kangle-3.5.8.tar.gz -O kangle-3.5.8.tar.gz -c
 tar xzf kangle-3.5.8.tar.gz
 cd kangle-3.5.8
 find|xargs touch
 ./configure --prefix=/vhs/kangle --enable-disk-cache --enable-ipv6 --enable-ssl --enable-vh-limit
 make
 make install
+wget --no-check-certificate https://raw.githubusercontent.com/viagram/docker/master/centos6-kangle/kanglestat -O /vhs/kangle/etc/kanglestat -c
 if [ -f /vhs/kangle/etc/kanglestat ] ; then
     chmod 775 /vhs/kangle/etc/kanglestat
     if [ ! -f /etc/init.d/kangle ] ; then
