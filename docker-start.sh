87e31c8da6c33611aeadde1c3a12ca626d047360
viagram <viagram@users.noreply.github.com>
2017-09-25 18:19:04 +0800

    Update

--- docker-start.sh
+++ docker-start.sh
@@ -10,34 +10,32 @@ __create_rundir() {
 	fi
 }
 __create_hostkeys() {
-	if [ ! -f /.ssh_key_set ]; then
-		if [ -f /etc/ssh/ssh_host_rsa_key ]; then
-			rm -f /etc/ssh/ssh_host_rsa_key
+	if [[ ! -f /.ssh_key_set ]]; then
+		if [[ -f /etc/ssh/ssh_host_ed25519_key ]]; then
+			rm -f /etc/ssh/ssh_host_ed25519_key
 		fi
-		if [ -f /etc/ssh/ssh_host_dsa_key ]; then
-			rm -f /etc/ssh/ssh_host_dsa_key
+		if [[ -f /etc/ssh/ssh_host_ed25519_key.pub ]]; then
+			rm -f /etc/ssh/ssh_host_ed25519_key.pub
 		fi
-		rm -f /etc/ssh/ssh_host_ed25519_key
-		rm -f /etc/ssh/ssh_host_ed25519_key.pub
 		(sleep 1;echo "y") | ssh-keygen -t dsa -f /etc/ssh/ssh_host_ed25519_key -N ""
-		chmod 620 /etc/ssh/moduli
-		chmod 644 /etc/ssh/ssh_config /etc/ssh/*.pub
-		chmod 600 /etc/ssh/sshd_config /etc/ssh/*_key
-		touch /.ssh_key_set 2>/dev/null
+		chmod 620 /etc/ssh/moduli >/dev/null 2>&1
+		chmod 644 /etc/ssh/ssh_config /etc/ssh/*.pub >/dev/null 2>&1
+		chmod 600 /etc/ssh/sshd_config /etc/ssh/*_key >/dev/null 2>&1
+		touch /.ssh_key_set >/dev/null 2>&1
 	fi
 }
 
 __change_pass() {
 	if [ ! -f /.user_pw_set ]; then
-		echo -e "$SSH_USERPASS" | (passwd --stdin $SSH_USERNAME) 2>/dev/null
-		touch /.user_pw_set 2>/dev/null
+		echo -e "$SSH_USERPASS" | (passwd --stdin $SSH_USERNAME) >/dev/null 2>&1
+		touch /.user_pw_set >/dev/null 2>&1
 		echo "========================================================================"
-		echo -e "						 部署完成"
+		echo -e "		部署完成"
 		echo 
-		echo -e "				  用户名称: $SSH_USERNAME"
-		echo -e "				  用户密码: $SSH_USERPASS"
+		echo -e "		用户名称: $SSH_USERNAME"
+		echo -e "		用户密码: $SSH_USERPASS"
 		echo 
-		echo -e "			警告: 初次登陆后, 记得修改密码!!!"
+		echo -e "		警告: 初次登陆后, 记得修改密码!!!"
 		echo "========================================================================"
 	fi
 }
